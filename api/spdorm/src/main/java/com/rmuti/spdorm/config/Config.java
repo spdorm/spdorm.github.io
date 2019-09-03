a6ff8f80739c53aa0c889217fcad7d5d3a3d0dda
Supanuch Hongsinee <supanuch.ho@gmail.com>
2019-09-03 21:31:06 +0700

    Update Config.java
    Update path for upload files.

--- api/spdorm/src/main/java/com/rmuti/spdorm/config/Config.java
+++ api/spdorm/src/main/java/com/rmuti/spdorm/config/Config.java
@@ -1,11 +1,7 @@
 package com.rmuti.spdorm.config;
 
 public class Config {
-//    dev
-//    public static final String DATA_PATH = "";
-
-//    production
-    public static final String DATA_PATH = "/home/networksoftwarecomputing/spdorm/data/api";
+    public static final String DATA_PATH = "/home/spdorm/images";
     public static final String DATA_PATH_DORM = DATA_PATH+"/dorm/";
     public static final String DATA_PATH_CHARTER = DATA_PATH+"/charter/";
 }
