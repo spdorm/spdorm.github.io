2827216d4adfe314a7d1da837b88a1f321135c38
Supanuch Hongsinee <supanuch.ho@gmail.com>
2019-09-05 17:54:55 +0700

    Update 05-09-2019

--- spdorm/lib/config.dart
+++ spdorm/lib/config.dart
@@ -1,7 +1,7 @@
 class config{
-  // For development
-  static const String API_url="http://spdorm.ddns.net:8080";
+// For development
+static const String API_url="http://spdorm.ddns.net:8080";
 
-  // For production
-//  static const String API_url="https://findbuild.co/api";
+// For production
+// static const String API_url="https://findbuild.co/api";
 }
