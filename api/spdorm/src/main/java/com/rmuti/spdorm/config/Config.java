26915479eae69f7d1ae3ede53fcc0c1bf987c244
Ekaphong Tangtrakul <39004906+EKAPHONG@users.noreply.github.com>
2019-09-07 10:58:58 +0700

    Update Config.java
    Change path from 'development' to 'production'

--- api/spdorm/src/main/java/com/rmuti/spdorm/config/Config.java
+++ api/spdorm/src/main/java/com/rmuti/spdorm/config/Config.java
@@ -2,12 +2,12 @@ package com.rmuti.spdorm.config;
 
 public class Config {
 // Path for development | พาธที่เก็บไฟล์ สำหรับใช้ระหว่างการพัฒนา
-    public static final String DATA_PATH = "/home/spdorm/images";
-    public static final String DATA_PATH_DORM = DATA_PATH+"/dorm/";
-    public static final String DATA_PATH_CHARTER = DATA_PATH+"/charter/";
-
-// Path for production | พาธที่เก็บไฟล์ สำหรับใช้งานจริง
-    // public static final String DATA_PATH = "/home/networksoftwarecomputing/spdorm/data/api";
+    // public static final String DATA_PATH = "/home/spdorm/images";
     // public static final String DATA_PATH_DORM = DATA_PATH+"/dorm/";
     // public static final String DATA_PATH_CHARTER = DATA_PATH+"/charter/";
+
+// Path for production | พาธที่เก็บไฟล์ สำหรับใช้งานจริง
+    public static final String DATA_PATH = "/home/networksoftwarecomputing/spdorm/data/api";
+    public static final String DATA_PATH_DORM = DATA_PATH+"/dorm/";
+    public static final String DATA_PATH_CHARTER = DATA_PATH+"/charter/";
 }
